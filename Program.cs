f7e827ebac8648dabcbcc4469750e1f88fba3f78
Primoz <sg.prima@gmail.com>
2020-08-30 16:59:34 +0200

    init setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace osm2json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
